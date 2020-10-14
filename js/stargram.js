84b18c015c31f6be29b02bdf8a4f71354e8cdca5
shabeerpayyoli <shabeerkeerankai@gmail.com>
2020-10-14 11:13:57 +0530

    first commit

---
+++ js/stargram.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".startTyper").typed({
+     strings: ["For Those Who are Talented & For Those Who believes In Your Talent .","One True Believer Is Worth More Than A Thousand Follower."],
+     typeSpeed: 30, // typing speed
+     backDelay: 1000, // pause before backspacing
+     loop: true, // loop on or off (true or false)
+     loopCount: false, // number of loops, false = infinite
+     });
+     });
